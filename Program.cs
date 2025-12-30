411ea7d660b7a5eb3b1958ce9d689a60e7edeb0e
kursat konasoglu <kursatkonasoglu0@gmail.com>
2025-12-30 04:24:05 +0300

    First upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO.Compression;
+
+class Program
+{
+    static void Main()
+    {
+        Bebek anakarakter = new Bebek("Küçük Führer");
+
+        Console.WriteLine($"{anakarakter} uyandı ve şuanki açlığı {anakarakter.aclik} şuanki uykusuzluğu {anakarakter.uykusuzluk}");
+        Console.WriteLine($"-----------------------------------------------------------------------------------------------------");
+
+        anakarakter.Uyudu();
+        anakarakter.Yemekye();
+
+
+
+    }
+
+    //sınıfımız
+    class Bebek
+    {
+        Random zar = new Random();
+        public string ad = "";
+        public int aclik;
+        public int uykusuzluk;
+
+
+        public Bebek(string adı)
+        {
+            ad = adı;
+            aclik = zar.Next(20, 101);
+            uykusuzluk = zar.Next(50, 101);
+
+        }
+
+        //metod1 
+        public void Yemekye()
+        {
+            aclik -= 20;
+            Console.WriteLine($"{ad} Yemek yedi şu anki açlığı {aclik}");
+
+        }
+
+        //metod2
+        public void Uyudu()
+        {
+            uykusuzluk -= 50;
+            Console.WriteLine($"{ad} bir 3 saatlik bir güzellik uykusu aldı geri kalan uykusuzluğu {uykusuzluk}");
+        }
+
+    }
+}
